e24e7826224c6cc08989ed5be1f8177713374fb0
CloudVisioner <mynewlife2778@gmail.com>
2025-11-13 21:03:33 +0900

    fix: organize homepage related sectional componets

---
+++ src/app/screens/homePage/AcitveUsers.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@mui/material";
+
+export default function AcitveUsers() {
+  return (
+    <div>
+      <Container>AcitveUsers</Container>
+    </div>
+  );
+}
---
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -0,0 +1,6 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export default function PopularDishes() {
+    return (<div><Container>PopularDishes</Container></div>)
+}
